eb13933583e373481cceb26446eb5cbe64543842
kh-dan <myfalseakk@gmail.com>
2021-12-02 16:35:20 +0200

    Added db in docker container

--- WebReservation.Data/Context/WebReservationContext.cs
+++ WebReservation.Data/Context/WebReservationContext.cs
@@ -7,15 +7,22 @@ namespace WebReservation.Data.Context
 {
     public class WebReservationContext : DbContext
     {
-        public DbSet<Reservation> Reservations { get; set; } // reservation 
+        public DbSet<Reservation> Reservations { get; set; } 
 
         public WebReservationContext(DbContextOptions<WebReservationContext> options): base(options)
         {
             
         }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) { }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(
+                "Server=localhost;Port=5432;Database=postgres;User Id=webadmin;Password=password;");
+        }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder) { }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+        }
     }
 }
